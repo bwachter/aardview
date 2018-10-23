7639b6a5b6253d71349fef2dea7cf8ac234951c4
Bernd Wachter <bwachter-spam@lart.info>
2018-10-23 14:37:24 +0300

    Bump to version

--- aardview/version.h
+++ aardview/version.h
@@ -1,5 +1,5 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define AARDVIEW_VERSION "0.3.6"
-#define AARDVIEW_GIT_SHA "faf276f"
+#define AARDVIEW_VERSION "0.3.7"
+#define AARDVIEW_GIT_SHA "a77e391"
 #endif
