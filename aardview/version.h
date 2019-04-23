4ff80c7da85b622abb63ddcad309105e675c836a
Bernd Wachter <bernd@wachter.fi>
2019-04-23 08:26:31 +0300

    Bump to version 0.3.10

--- aardview/version.h
+++ aardview/version.h
@@ -1,5 +1,5 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define AARDVIEW_VERSION "0.3.9"
-#define AARDVIEW_GIT_SHA "0babb91"
+#define AARDVIEW_VERSION "0.3.10"
+#define AARDVIEW_GIT_SHA "97bff75"
 #endif
