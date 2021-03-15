11e0559c331dffc0faf86fbd73b80853f349d385
Bernd Wachter <bernd@wachter.fi>
2021-03-15 21:25:42 +0000

    Bump to version 0.3.11

--- aardview/version.h
+++ aardview/version.h
@@ -1,5 +1,5 @@
 #ifndef VERSION_H
 #define VERSION_H
-#define AARDVIEW_VERSION "0.3.10"
-#define AARDVIEW_GIT_SHA "97bff75"
+#define AARDVIEW_VERSION "0.3.11"
+#define AARDVIEW_GIT_SHA "2be6f24"
 #endif
